8606aaa380d1e6e9143b7f52bf692e39c8691623
Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2024-05-14 12:57:45 +0700

    feat: Add Button component and update landing page layout

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button className="" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
