15aae4dfcad7efed27ba9aea1df07679bf9b683a
Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2024-05-14 16:02:48 +0700

    chore: Update font families in global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,26 @@
-import type { Metadata } from "next";
+import { Roboto, Roboto_Mono, Roboto_Slab } from "next/font/google";
+import { Metadata } from "next";
+
 import "./globals.css";
+import { cn } from "@/lib/cn";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+const robotoMono = Roboto_Mono({
+  weight: "400",
+  style: "normal",
+  subsets: ["latin"],
+});
+
+const robotoSlab = Roboto_Slab({
+  weight: ["300", "400", "500", "700"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | In-browser markdown editor",
@@ -12,7 +33,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          roboto.className,
+          robotoMono.className,
+          robotoSlab.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
